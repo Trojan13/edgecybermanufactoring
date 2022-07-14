41ef13448f8242b6c9142c03cbee4c77023b7702
Tim Pack <tim_colin.pack1@smail-th-koeln.de>
2022-07-14 12:38:43 +0200

    Tests and data

---
+++ data_analysis/train.py
@@ -0,0 +1,64 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+print(f"TensorFlow version = {tf.__version__}\n")
+
+# Set a fixed random seed value, for reproducibility, this will allow us to get
+# the same random numbers each time the notebook is run
+SEED = 1337
+np.random.seed(SEED)
+tf.random.set_seed(SEED)
+
+# the list of MOVEMENTs that data is available for
+MOVEMENTS = [
+    "middle",
+    "much",
+    "no",
+]
+
+SAMPLES_PER_MOVEMENT = 45
+
+NUM_MOVEMENTS = len(MOVEMENTS)
+
+# create a one-hot encoded matrix that is used in the output
+ONE_HOT_ENCODED_MOVEMENTS = np.eye(NUM_MOVEMENTS)
+
+inputs = []
+outputs = []
+
+# read each csv file and push an input and output
+for MOVEMENT_index in range(NUM_MOVEMENTS):
+  MOVEMENT = MOVEMENTS[MOVEMENT_index]
+  print(f"Processing index {MOVEMENT_index} for MOVEMENT '{MOVEMENT}'.")
+  
+  output = ONE_HOT_ENCODED_MOVEMENTS[MOVEMENT_index]
+  
+  df = pd.read_csv("data_" + MOVEMENT + "_motion.csv",sep=',')
+  
+  # calculate the number of MOVEMENT recordings in the file
+  num_recordings = int(df.shape[0] / SAMPLES_PER_MOVEMENT)
+  
+  print(f"\tThere are {num_recordings} recordings of the {MOVEMENT} MOVEMENT.")
+  
+  for i in range(num_recordings):
+    tensor = []
+    for j in range(SAMPLES_PER_MOVEMENT):
+      index = i * SAMPLES_PER_MOVEMENT + j
+      # normalize the input data, between 0 to 1:
+      # - acceleration is between: -4 to +4
+      tensor += [
+          (df['accel_x'][index] + 4) / 8,
+          (df['accel_y'][index] + 4) / 8,
+          (df['accel_z'][index] + 4) / 8,
+      ]
+
+    inputs.append(tensor)
+    outputs.append(output)
+
+# convert the list to numpy array
+inputs = np.array(inputs)
+outputs = np.array(outputs)
+
+print("Data set parsing and preparation complete.")
---
+++ esp8266_display_sensor_mqtt/esp8266_display_sensor_mqtt.ino
@@ -0,0 +1,97 @@
+#include <Arduino.h>
+#include <U8x8lib.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+
+const char *mqtt_broker = "192.168.178.53";
+const char *topic = "acceleration-test";
+const char *mqtt_username = "test";
+const char *mqtt_password = "test";
+const int mqtt_port = 1883;
+
+const char *wifi_ssid = "";
+const char *wifi_password = "";
+
+String displayed_message = "";
+String current_status = "";
+// Pin 16   OLED Reset
+// Pin 4,5  I2C
+
+// use Board "Wemos D1 Mini R2", 80Mhz, 4(3)MB Flash, 921600baud to flash
+U8X8_SSD1306_128X32_UNIVISION_HW_I2C u8x8(16);
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+  Serial.print("Message:");
+  displayed_message = "";
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+    displayed_message += (int)payload[i];
+  }
+
+  float aFloat = atof(displayed_message);
+  if (aFloat > 2.5 && aFloat < 3.5)
+  {
+    current_status = "low";
+  }
+  else if (aFloat > 3.5 && aFloat < 6.0)
+  {
+    current_status = "middle";
+  }
+  else if (aFloat > 6.0 && aFloat < 3.5)
+  {
+    current_status = "high";
+  }
+  u8x8.clear();
+  u8x8.setFont(u8x8_font_amstrad_cpc_extended_r);
+  u8x8.draw2x2String(0, 2, current_status.c_str());
+}
+
+void setup()
+{
+  // Set software serial baud to 115200;
+  Serial.begin(115200);
+  // connecting to a WiFi network
+  WiFi.begin(wifi_ssid, wifi_password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+  // connecting to a mqtt broker
+  client.setServer(mqtt_broker, mqtt_port);
+  client.setCallback(callback);
+  while (!client.connected())
+  {
+    String client_id = "esp8266-client-";
+    client_id += String(WiFi.macAddress());
+    Serial.printf("The client %s connects to the mqtt broker\n", client_id.c_str());
+    if (client.connect(client_id.c_str(), mqtt_username, mqtt_password))
+    {
+      Serial.println("mqtt broker connected");
+    }
+    else
+    {
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
+
+  u8x8.begin();
+  // subscribe
+  client.subscribe(topic);
+}
+
+void loop(void)
+{
+  client.loop();
+  delay(1000);
+}
